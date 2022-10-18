cc179f47ac085b969229122638b7395d21a719f3
Deepak Aggarwal <deepak.aggarwal9@gmail.com>
2022-10-18 22:32:28 +0530

    Delete certificate

--- certificate/urls.py
+++ certificate/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import get_certificates, request_certificates, check_certificate, get_certificate_schemas, get_certificate_schema_attributes
+from .views import get_certificates, request_certificates, check_certificate, get_certificate_schemas, get_certificate_schema_attributes, delete_certificate
 
 urlpatterns = [
         path('/', get_certificates),
@@ -8,4 +8,5 @@ urlpatterns = [
         path('/check', check_certificate),
         path('/schema/attributes', get_certificate_schema_attributes),
         path('/schema', get_certificate_schemas),
+        path('/delete', delete_certificate),
 ]
