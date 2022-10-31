89f0f9781db6d86786493c4666908a3d2c479bb0
George J Padayatti <george.padayatti@igrant.io>
2022-10-31 17:10:32 +0530

    Update user modal
    Added columns 1. org 2. org_verification_status
    
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- igrant_user/models.py
+++ igrant_user/models.py
@@ -2,21 +2,40 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from .managers import IGrantUserManager
+
+from igrant_user.managers import IGrantUserManager
 
 
 class IGrantUser(AbstractBaseUser, PermissionsMixin):
     class UserType(models.TextChoices):
-        COMPANY = 'COMPANY', _('Company')
-        ISSUER = 'ISSUER', _('Issuer')
+        COMPANY = "COMPANY", _("Company")
+        ISSUER = "ISSUER", _("Issuer")
 
-    email = models.EmailField(_('email address'), unique=True)
+    class Orgs(models.TextChoices):
+        DEFAULT = "NIL", _("Nil")
+        RAKSA_OY = "RAKSA_OY", _("Raksa Oy, Finland")
+        BYGG_AB = "BYGG_AB", _("Bygg Ab, Sweden")
+        BOLAGSVERKET = "BOLAGSVERKET_AB", _("Bolagsverket Ab, Sweden")
+
+    class OrgsVerificationStatus(models.TextChoices):
+        UNVERIFIED = "UNVERIFIED", _("Unverified")
+        VERIFIED = "VERIFIED", _("Verified")
+
+    email = models.EmailField(_("email address"), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
-    user_type = models.CharField(max_length=8, choices=UserType.choices, default=UserType.COMPANY)
+    user_type = models.CharField(
+        max_length=8, choices=UserType.choices, default=UserType.COMPANY
+    )
+    org = models.CharField(max_length=250, choices=Orgs.choices, default=Orgs.DEFAULT)
+    org_verification_status = models.CharField(
+        max_length=250,
+        choices=OrgsVerificationStatus.choices,
+        default=OrgsVerificationStatus.DEFAULT,
+    )
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = IGrantUserManager()
