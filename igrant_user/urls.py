9fba33baa111eeed873d0c685dfc4b1ab78a2e02
Albin Antony <albin.pa3@gmail.com>
2022-11-08 11:03:02 +0530

    Add #29 Add Delete API to reset connection_id, connection_state, presentation_exchange_id, presentation_state, presentation_record

--- igrant_user/urls.py
+++ igrant_user/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import UserList, UserDetail
+from .views import UserList, UserDetail, AdminResetConnection
 
 urlpatterns = [
         path('/', UserList.as_view()),
         path('/<int:pk>/', UserDetail.as_view()),
+        path('/admin/reset-connection', AdminResetConnection),
 ]
--- igrant_user/views.py
+++ igrant_user/views.py
@@ -3,6 +3,9 @@ from .models import IGrantUser
 from .serializers import IGrantUserSerializer
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 
 
 # Create your views here.
@@ -19,3 +22,17 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = IGrantUser.objects.all()
     serializer_class = IGrantUserSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+
+@permission_classes([permissions.IsAdminUser])
+@api_view(["POST"])
+def AdminResetConnection(request):
+    queryset = IGrantUser.objects.all()
+    for user in queryset:
+        user.connection_id = None
+        user.connection_state = None
+        user.presentation_exchange_id = None
+        user.presentation_state = None
+        user.presentation_record = []
+        user.save()
+    return HttpResponse(status=status.HTTP_200_OK)
