73c1aecbf1a77757782a4815e2d6a2b06e7a0ec5
George J Padayatti <george.padayatti@igrant.io>
2022-11-13 15:46:57 +0530

    Update presentation_record column to accept null values
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- seller/models.py
+++ seller/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from django_jsonfield_backport.models import JSONField
-from buyer.models import Tender, Requirement
+
+from buyer.models import Requirement, Tender
 from igrant_user.models import IGrantUser
 
 
 # Create your models here.
 class Responses(models.Model):
     tender = models.ForeignKey(Tender, on_delete=models.CASCADE, blank=True, null=True)
-    requirements = models.ForeignKey(Requirement, on_delete=models.CASCADE, blank=True, null=True)
-    supplier = models.ForeignKey(IGrantUser, on_delete=models.CASCADE, blank=True, null=True)
-    presentation_exchange_id = models.CharField(max_length=250,unique=True,null=True,blank=True)
-    presentation_state = models.CharField(max_length=250,null=True,blank=True)
-    presentation_record = JSONField(default=[],blank=True)
-    
-    
+    requirements = models.ForeignKey(
+        Requirement, on_delete=models.CASCADE, blank=True, null=True
+    )
+    supplier = models.ForeignKey(
+        IGrantUser, on_delete=models.CASCADE, blank=True, null=True
+    )
+    presentation_exchange_id = models.CharField(
+        max_length=250, unique=True, null=True, blank=True
+    )
+    presentation_state = models.CharField(max_length=250, null=True, blank=True)
+    presentation_record = JSONField(default=[], blank=True, null=True)
